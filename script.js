19bcc5c2bd27f3389f8d9597c37662991176c9fc
Devlinker <mathansaran8@gmail.com>
2024-07-19 21:45:52 +0530

    ADDED NEW FILE

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+     LOOP Methods
+
+for(count=1;count<=10;count=count+1)
+   {
+      console.log(count+"*2="+count*2)
+   } */
+
+
+      var fruit = ["Apple","Orange","Grape","Banana"]
+      console.log(fruit.length)
+
+      for(count=0;count<fruit.length;count=count+1)
+      {
+         console.log(fruit[count])
+      }
