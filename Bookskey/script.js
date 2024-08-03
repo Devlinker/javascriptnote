2cb9246f3c62996f453ade24c100fd494184f6c7
Devlinker <mathansaran8@gmail.com>
2024-08-03 11:30:32 +0530

    added new title popup box

--- Bookskey/script.js
+++ Bookskey/script.js
@@ -22,56 +22,27 @@ cancelpopup.addEventListener("click",function(event){
 
 //selecting container, add-book, book-title-input, book-author-input, book-description-input
 
-//var container = document.querySelector(".container")
-//var addbook = document.getElementById("add-book")
-//var booktitleinput = document.getElementById("book-title-input")
-//var bookauthorinput = document.getElementById("book-author-input")
-//var bookdescriptioninput = document.getElementById("book-description-input")
+var container = document.querySelector(".container")
+var addbook = document.getElementById("add-book")
+var booktitleinput = document.getElementById("book-title-input")
+var bookauthorinput = document.getElementById("book-author-input")
+var bookdescriptioninput = document.getElementById("book-description-input")
 
-//addbook.addEventListener("click",function(event){
-    //event.preventDefault()
-    //var div = document.createElement("div")
-    //div.setAttribute("class","book-container")
-    //div.innerHTML = `<h2>${booktitleinput.value}</h2>`
-    //container.append(div)
-    //popupoverlay.style.display = "none"
-    //popupbox.style.display = "none"
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
 
-
-//})
-
-
-
-/*var container = document.querySelector(".container");
-var addbook = document.getElementById("add-book");
-var booktitleinput = document.getElementById("book-title-input");
-var bookauthorinput = document.getElementById("book-author-input");
-var bookdescriptioninput = document.getElementById("book-description-input");
-
-// Assuming popupoverlay and popupbox are defined somewhere
-var popupoverlay = document.getElementById("popup-overlay");
-var popupbox = document.getElementById("popup-box");
-
-addbook.addEventListener("click", function(event){
-    event.preventDefault();
-    
     // Create the book container div
-    var div = document.createElement("div");
-    div.setAttribute("class", "book-container");
-    
-    // Set the inner HTML of the book container
-    div.innerHTML = `
-        <h2>${booktitleinput.value}</h2>
-        <h3>Author: ${bookauthorinput.value}</h3>
-        <p>${bookdescriptioninput.value}</p>
-    `;
-    
-    // Append the new book container to the main container
-    container.append(div);
-    
-    // Hide the popup
-    popupoverlay.style.display = "none";
-    popupbox.style.display = "none";
-})  
+    var div = document.createElement("div")
+    div.setAttribute("class","book-container")
 
-*/
+   // Set the inner HTML of the book container
+        div.innerHTML = `<h2>${booktitleinput.value}</h2>
+        <h5> ${bookauthorinput.value}</h5>
+        <p>${bookdescriptioninput.value}</p>
+        <button class="buy-button">Delete</button>`
+    container.append(div)
+
+    // Hide the popup
+    popupoverlay.style.display = "none"
+    popupbox.style.display = "none"
+})
